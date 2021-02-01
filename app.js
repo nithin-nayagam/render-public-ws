d2c96fdd71ebda049d62cd55909db41a2d3fba92
Nithin Nayagam A C <nithin.nayagam@freshworks.com>
2021-02-01 15:23:37 +0530

    send as json

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.send({ apps: ['whatsapp', 'instagram', 'sync with freshsales'] }));
+app.get("/", (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ apps: ['whatsapp', 'instagram', 'salesforce'] }));
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
